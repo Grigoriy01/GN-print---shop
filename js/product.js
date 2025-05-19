aef0fcac220601c107072f0afcddbca78020c8d5
Grigoriy01 <100162884+Grigoriy01@users.noreply.github.com>
2025-05-19 10:21:44 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -140,13 +140,14 @@ window.renderCart = renderCart;
 
 // === Отрисовка карточки товара
 document.addEventListener("DOMContentLoaded", () => {
-  // Если пользователь вернулся назад из PayPal, перенаправляем на главную
-  if (document.referrer.includes("paypal.") || sessionStorage.getItem("orderSubmitted") === "1") {
-    localStorage.removeItem("cart");                  // очищаем корзину
-    sessionStorage.removeItem("orderSubmitted");      // удаляем флаг
-    window.location.href = "index.html";              // отправляем на стартовую страницу
-  }
-  
+  const slug = new URLSearchParams(window.location.search).get("slug");
+   // Если пользователь вернулся назад из PayPal, перенаправляем на главную
+    if (document.referrer.includes("paypal.") || sessionStorage.getItem("orderSubmitted") === "1") {
+      localStorage.removeItem("cart");                  // очищаем корзину
+      sessionStorage.removeItem("orderSubmitted");      // удаляем флаг
+      window.location.href = "index.html";              // отправляем на стартовую страницу
+    }
+
 
 
 
