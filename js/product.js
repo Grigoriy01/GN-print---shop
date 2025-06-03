05707038275124f5c4a14fe8af8fcc6b81ddf4a9
Grigoriy01 <100162884+Grigoriy01@users.noreply.github.com>
2025-06-03 20:17:01 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -426,7 +426,7 @@ document.addEventListener("submit", (e) => {
         notes: data.notes
       };
 
-      fetch("https://proxy-printshop.m9258923028.workers.dev/?url=https://script.google.com/macros/s/AKfycby8avH7o6ahtOt0COF0jymg073YDtKmz3HgCrmjuu3og0_2ip3XIIInlODvrI9Jahg6CA/exec", {
+      fetch("https://proxy-printshop.m9258923028.workers.dev/?url=https://script.google.com/macros/s/AKfycbyPsWIINa4FdIy62ULSl9Y3Adr9Byo53SxpXFbdvoNiGbHB-k_GRHYubJ0QBbQEXEvxfA/exec", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
